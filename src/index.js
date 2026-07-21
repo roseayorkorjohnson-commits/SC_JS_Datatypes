289682ee1c14de5b75a233a71d4d272971572678
roseayorkorjohnson <roseayorkorjohnson@gmail.com>
2026-07-21 06:51:59 -0700

    Added changes

--- src/index.js
+++ src/index.js
@@ -2,59 +2,82 @@
 // JavaScript has different types of data.
 
 // i. String (Text surrounded by quotes)
-let country = "Ghana"; 
+const myString ="Hello Kitty";
+let myZazzy ="Alex";
+let myCat ="Mittens";
+let myDog ="Buddy";
+let randomNo ="8"; //This is a string
+let status ="true"; // This is a sting
 
 // ii. Number (Whole numbers or decimals, no quotes)
-let age = 21; 
-let price = 19.99; 
+let no1 = 8; //This is a number
 
 // iii. Boolean (True or false logical values, no quotes)
-let isClassOver = false; 
+let available = true; // This is a boolen value
+available = false; // This is also a boolen value
 
 // iv. Null (Intentional emptiness)
-let emptyWallet = null; 
+let empty = "";
+
+console.log(myZazzy); //output - Alex
+myZazzy ="Chairman Wontumi";
+console.log(myZazzy); // output - Chairman Wontumi
+let userNum = prompt("Give me number from 1 - 100:");
+console.log(userNum); // output - 89
+console.log(typeof(userNum)); // output - number
+let myNum  = 10;
+console.log(userNum + myNum); //99
 
 // TASK 2: Use the typeof function to check data types in the console
-console.log(typeof country);     // Prints: string
-console.log(typeof age);         // Prints: number
-console.log(typeof isClassOver); // Prints: boolean
-console.log(typeof emptyWallet); // Prints: object (This is a well-known JS bug, but it means null!)
-
+ console.log (typeof(myDog)); // output or print: string
+console.log (typeof(8)); // output or print: number
 
 
 // TASK 3: SIMPLE CALCULATIONS (MATH OPERATORS)
+let x = 25;
+let y = 50;
 
-let x = 10;
-let y = 3;
 
 // i. Addition (+)
-let sum = x + y;
-console.log(sum); // Prints: 13
+let addNumbers = x + y;
+console.log(addNumbers); // output - 75
+console.log(x+y); // output - 75
 
 // ii. Subtraction (-)
-let difference = x - y;
-console.log(difference); // Prints: 7
+console.log(y-x); // output - 25
 
 // iii. Multiplication (*)
-let product = x * y;
-console.log(product); // Prints: 30
+let productNumbers = x * y;
+console.log(productNumbers); //output - 1250
 
 // iv. Division (/)
-let quotient = x / y;
-console.log(quotient); // Prints: 3.3333333333333335
+let divideNumbers = y/x;
+console.log(divideNumbers); //output is 2
 
 // v. Remainder using Modulo (%)
 // Explaining Modulo: How many times does 3 fit into 10? It fits 3 times (which is 9), with 1 left over.
-let remainder = x % y;
-console.log(remainder); // Prints: 1
+let remainder = y % x;
+console.log(remainder); //output 0
+
+x = 3;
+y = 5;
+
+remainder = y % x;
+console.log(remainder); // 2
+productNumbers = x * y;
+divideNumbers = y/x;
+remainder = y % x;
+
 
 
 // TASK 4 & 5: CONSOLE.LOG & STRING INTERPOLATION
 // Task 4: Using console.log to print basic messages
-console.log("Testing our VS Code connection to the browser console!");
+console.log("Testing our VSCode connections to the Browser Console");
 
 // Task 5: Practice using string interpolation (backticks) to log from VS Code
-let itemName = "Laptop";
-let itemPrice = 1200;
+console.log(`Today, we learnt how to add, substract, multiply, divide and find the remainder of 2 numbers.Given that x is ${x} and y is ${y}, the summ will be ${addNumbers}
+ the difference will be ${y-x}, the product will be ${productNumbers}, the quotient will be ${divideNumbers} and the remainder will
+e sum will be ${remainder}`);
 
-console.log(`The price of the new ${itemName} is $${itemPrice}.`);
+
+            
